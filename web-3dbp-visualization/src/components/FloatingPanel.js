205da9f55f77040dc0d874c29cdd30bc5ac77640
Yamil Mateo Rodríguez <yamilmateor@gmail.com>
2021-05-21 02:32:06 +0200

    Added table without css

--- web-3dbp-visualization/src/components/FloatingPanel.js
+++ web-3dbp-visualization/src/components/FloatingPanel.js
@@ -9,6 +9,20 @@ import Col from 'react-bootstrap/Col';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function FloatingPanel(props) {
+    const [itemData, setItemData] = useState({
+        data: []
+    })
+
+    useEffect(() => {
+        var d = []
+        for (const [key, value] of Object.entries(props.selectedItem.item)) {
+            if (key !== "pp_in" && key !== "pp_out" && key !== "subzones" && key !== "mass_center") {
+                d.push([key, value])
+            }
+        }
+        console.log(d)
+        setItemData({ data: d })
+    }, [props.selectedItem])
     return (
         <Container>
             <Row style={{ border: '2px solid black', borderRadius: 8, height: '25vh' }}>
@@ -17,7 +31,7 @@ export default function FloatingPanel(props) {
                         borderRadius: 8,
                         borderColor: 'black',
                     }}>
-                    <PerspectiveCamera makeDefault position={[0, 0, 1]} props={{ aspect: 2 }}></PerspectiveCamera>
+                    <PerspectiveCamera makeDefault position={[0, 0, 1.3]}></PerspectiveCamera>
                     <ambientLight intensity={1.5} />
                     {console.log(props.selectedItem)}
                     <ProjectedBox
@@ -25,6 +39,10 @@ export default function FloatingPanel(props) {
                         color={props.color} />
                 </Canvas>
             </Row>
+            <Table
+                columns={["Attribute", "Value"]}
+                data={itemData.data} />
+
             <Row>
 
             </Row>
